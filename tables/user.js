eeeff46929a1594733e25ade6ec42862442e4887
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-24 21:15:22 +0300

    added user table

---
+++ tables/user.js
@@ -0,0 +1,8 @@
+const createUserTableQuery = `
+CREATE TABLE IF NOT EXISTS Users (
+  Id INT AUTO_INCREMENT PRIMARY KEY,
+  Username VARCHAR(255) UNIQUE NOT NULL,
+  Password VARCHAR(255) NOT NULL
+);
+`;  
+module.exports = createUserTableQuery;
