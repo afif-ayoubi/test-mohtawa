dd4e29301eb14944f5f760fc0cb1008dc8b3a032
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-24 20:39:17 +0300

    initialized the book router

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 require("./configs/db.config.js");
 require("dotenv").config();
 const app = express();
-
+const bookRouter = require("./routes/book.routes");
+app.use("/api/books", bookRouter);
 app.use(express.json());
 app.listen(3001, (err) => {
   if (err) throw err;
