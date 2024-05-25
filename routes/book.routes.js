c407496c581214c2d504e822a21ae2a6a750ce31
afif-ayoubi <afifayoubi22@gmail.com>
2024-05-25 17:00:48 +0300

    added all routes

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
-
 const router = express.Router();
-module.exports = router;  
+const bookController = require("../controllers/book.controller");
+
+router.get("/", bookController.getAllBooks);
+router.get("/:id",  bookController.getBookById);
+router.post("/",  bookController.addBook);
+router.put("/:id", bookController.updateBook);
+router.delete("/:id", bookController.deleteBook);
+
+module.exports = router;
